cfb440106b7829ceaf9366a29cc8d85c1cd8ab77
caio.borsoi <caio.borsoi@yetgo.com.br>
2017-06-18 19:23:00 -0300

    -- item quantities in ProductDetail

--- OnMarketBR/ProdutoDetailViewController.swift
+++ OnMarketBR/ProdutoDetailViewController.swift
@@ -15,13 +15,21 @@ class ProdutoDetailViewController: UIViewController {
     @IBOutlet weak var productImage: UIImageView!
     @IBOutlet weak var productName: UILabel!
     @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productQuantity: UILabel!
+    @IBOutlet weak var productTotal: UILabel!
     var product: Product?
+    var itensQuantity: Double = 1 {
+        didSet {
+            productQuantity.text = "\(itensQuantity)"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         self.navigationController?.navigationBar.tintColor = UIColor.white
         settingUI()
+        itensQuantity = 1
     }
     
     func settingUI(){
@@ -32,6 +40,7 @@ class ProdutoDetailViewController: UIViewController {
             }
             productName.text = produto.name
             productPrice.text = "Unidade - \(produto.display_price ?? "num veio nada")"
+            productTotal.text = produto.display_price
         }
     }
     
@@ -39,6 +48,21 @@ class ProdutoDetailViewController: UIViewController {
         SideMenuManager.menuRightNavigationController = storyboard!.instantiateViewController(withIdentifier: "RightMenuNavigationController") as? UISideMenuNavigationController
         SideMenuManager.menuAddPanGestureToPresent(toView: self.navigationController!.navigationBar)
         SideMenuManager.menuAddScreenEdgePanGesturesToPresent(toView: self.navigationController!.view)
+    }
+    @IBAction func addBtnPressed(sender: UIButton) {
+        itensQuantity += 1
+        if let produto = product{
+            //productTotal.text = produto.display_price
+           // productTotal.text = itensQuantity*produto.price
+            
+            productTotal.text = "\(itensQuantity * produto.price!)"
+        }
+    }
+    @IBAction func minusBtnPressed(sender: UIButton) {
+        itensQuantity -= 1
+        if let produto = product{
+            productTotal.text = "\(itensQuantity * produto.price!)"
+        }
     }
 
 }
